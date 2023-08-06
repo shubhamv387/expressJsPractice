84302f54bfeab0b5db9a8f1ecb95e612750e4cd8
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-08-06 12:09:22 +0530

    added body-parser module

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const http = require("http");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.use((req, res, next) => {
-  console.log("in the middleware!");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  //   console.log("in another middleware!");
+  res.send(
+    "<form action ='/product' method='POST'><input type='text' name='title' placeholder='product name' /><br><input type='number' name='quantity' placeholder='QTY' /><br><input type='submit' value='submit' /></form>"
+  );
 });
-app.use((req, res, next) => {
-  console.log("in another middleware!");
-  // res.send("<h1>Hello from express!</h1>"); // Content-Type = text/html
-  res.send({ key1: "value" }); // Content-Type = application/json
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body.title);
+  console.log(req.body.quantity);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  //   console.log("in another middleware!");
+  res.send("<h1>Hello from express!</h1>");
 });
 
 app.listen(3000);
