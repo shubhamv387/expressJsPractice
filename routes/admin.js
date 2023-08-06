56250f60a3c2ca6a414116eda1028d8a3ab9ede7
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-08-06 20:12:37 +0530

    fixed add-product issue

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,16 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-const rootDir = require("../util/path");
+// const rootDir = require("../util/path");
 
 router.get("/add-product", (req, res, next) => {
   //   console.log("in another middleware!");
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
-  // console.log(req.body.title);
-  // console.log(req.body.quantity);
+  console.log(req.body.title);
+  console.log(req.body.quantity);
   res.redirect("/shop");
 });
 
